10a2ee2ae132ec1889e76bfd01799d074aacd36b
oshoura <omarshoura1@gmail.com>
2025-05-03 11:14:41 -0400

    better color

--- client/src/components/ui/button/Button.vue
+++ client/src/components/ui/button/Button.vue
@@ -24,7 +24,7 @@ const buttonClass = computed(() => {
   return cn(
     'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
     {
-      'bg-primary text-primary-foreground hover:bg-primary/90': props.variant === 'default',
+      'bg-orange-500 text-slate-100 hover:bg-orange-600': props.variant === 'default',
       'bg-destructive text-destructive-foreground hover:bg-destructive/90': props.variant === 'destructive',
       'border border-input bg-background hover:bg-accent hover:text-accent-foreground': props.variant === 'outline',
       'bg-secondary text-secondary-foreground hover:bg-secondary/80': props.variant === 'secondary',
